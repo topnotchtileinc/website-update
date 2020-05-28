5eaf94403b165b618d68486260b7fb24bb49b6cb
Vanessa <ignacio@topnotchtileinc.com>
2020-05-28 14:42:38 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/** $(function () {
+    var selectedClass = "";
+    $(".filter").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+}); **/
+
+
+
+  $(function () {
+    $(".btn").click(function(){
+        alert("The paragraph was clicked.");
+      });
+});
